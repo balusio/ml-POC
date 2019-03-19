35788aca06f981aa05438f41c52f9b644e0298ec
Jorge Manzano <jmanzano@everfi.com>
2019-03-19 02:46:29 -0300

    UI Added

---
+++ ui/src/components/app.js
@@ -0,0 +1,4 @@
+console.log('balo was here');
+import React from 'react';
+
+export default () => <h1>Hello!</h1>;
---
+++ ui/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // devServer configure
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4200
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
