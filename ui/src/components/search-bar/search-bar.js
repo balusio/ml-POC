25ebd576235ab87f10a23d185a211479d290313e
Jorge Manzano <jmanzano@everfi.com>
2019-03-22 03:01:41 -0300

    scss varibales and search results

--- ui/src/components/search-bar/search-bar.js
+++ ui/src/components/search-bar/search-bar.js
@@ -1,23 +1,30 @@
 
 import React, { Component } from "react";
+import SearchForm from './search-form/search-form';
 import './search-bar.scss';
 import lupa from '../../assets/ic_Search.png';
 import lupa2x from '../../assets/ic_Search@2x.png';
+import logo_ML from '../../assets/logo_ML.png';
+import logo_ML2x from '../../assets/logo_ML@2x.png';
 class SearchBar extends Component {
   constructor() {
     super();
     this.state = {
-      title: "hello washioooo"
+      title: "hello washioooo",
+      logo: logo_ML
     };
   }
   render() {
     return (
-      <div className="search-bar">
-        <div className="container">
-          <img src={lupa} srcSet={lupa2x} /> 
+      <nav className="container container-search">
+        <div className="search-bar wrapper">
+          <div className="logo">
+            <img src={this.props.logo} srcSet={logo_ML2x}  alt="mercado-libre"/>
+          </div>
+          <SearchForm />
           <p>{this.props.title}</p>
         </div>
-      </div>
+      </nav>
       
     );
   }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar/search-bar';
+import SearchResult from './components/search-result/search-result';
 
 import './styles/main.scss';
 const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
-  <SearchBar></SearchBar>,
+  <div>
+  <SearchBar></SearchBar>
+  <SearchResult></SearchResult>
+  </div>,
   document.getElementById('app')
 );
