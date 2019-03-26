7bbb1a204c9d4b5c695f1afd03bf633e3838b200
Jorge Manzano <jmanzano@everfi.com>
2019-03-26 03:07:53 -0300

    handled form query

--- ui/src/components/search-bar/search-form/search-form.js
+++ ui/src/components/search-bar/search-form/search-form.js
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 import './search-form.scss';
 import lupa from '../../../assets/ic_Search.png';
 import lupa2x from '../../../assets/ic_Search@2x.png';
+import { Redirect } from 'react-router';
 import {Link} from 'react-router-dom';
 class SearchForm extends Component {
-  
+  constructor(props) {
+    super(props);
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      searchQuery: '',
+    };
+   
+  }
+  handleChange(event){
+    this.setState({searchQuery: event.target.value});
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(this.state.searchQuery);
+    const query = `/items?search=${this.state.searchQuery}`;
+    this.props.history.push(query);
+    
+  }
+
+
   render() {
     return(
-      <form className="search-box">
-        <input className="input-search" placeholder="Nunca dejes de buscar" />
-        <Link to="/items">
-          <button type="submit" id="searchButton">
+      <form className="search-box" onSubmit={this.handleSubmit}>
+        <input className="input-search" placeholder="Nunca dejes de buscar" value={this.state.searchQuery} onChange={this.handleChange}  />
+          <button type="submit" id="searchButton" >
             <img src={lupa} srcSet={lupa2x} alt="buscar"/> 
           </button> 
-        </Link> 
       </form>
     );
   }
